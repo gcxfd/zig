19cbf8f86cbc22a4fa082e8926df6f0879c98ac6
gcxfd <i@rmw.link>
2022-07-15 17:28:25 +0800

    🔶

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,9 @@ const ziglyph = @import("ziglyph");
 const std = @import("std");
 
 pub fn main() anyerror!void {
+    const txt = "banana";
+    std.log.info("> {s}", .{txt});
+
     std.log.info("All your codebase are belong to us.", .{});
 }
 
