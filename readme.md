9b0f7a4019ed34a88db05ad7341b6c35ca62c49c
gcxfd <i@rmw.link>
2022-07-15 17:39:33 +0800

    🔶

--- readme.md
+++ readme.md
@@ -7,6 +7,7 @@
 
 ## 参考文献
 
+[zig 代码范例](https://ziglang.org/zh/learn/samples/)
 [zig 入门教程](https://ziglearn.org/chapter-1/)
 [嵌入式编程 zig 篇 (三）zig 的内存分配器](https://zhuanlan.zhihu.com/p/447482200)
 [Zig 学习篇 |zig 生成和调用静态库与动态库](https://zhuanlan.zhihu.com/p/457791672)
--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,20 @@
 const ziglyph = @import("ziglyph");
 const std = @import("std");
 
+const fixedBufferStream = std.io.fixedBufferStream;
+const AllKeysFile = ziglyph.collator.AllKeysFile;
+
 pub fn main() anyerror!void {
-    const txt = "banana";
+    var allocator = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(!allocator.deinit());
+    const arena = allocator.allocator();
+
+    const txt = "The most common newbie mistake: \nSo while you're learning Zig you come up with the following program:";
     std.log.info("> {s}", .{txt});
+
+    var fbs = fixedBufferStream(&txt);
+
+    AllKeysFile.parse(arena, fbs.reader());
 
     std.log.info("All your codebase are belong to us.", .{});
 }
